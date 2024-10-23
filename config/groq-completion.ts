153aa0c4b83b127b824d9f6f91d2d2e339bb7d67
Piyush Yadav <piyushyadav0191@gmail.com>
2024-10-23 09:55:40 +0530

    chore: capable of streaming response

--- config/groq-completion.ts
+++ config/groq-completion.ts
@@ -3,13 +3,23 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const getGroqCompletion = async (messages: any[], model="gemma2-9b-it") => {
+    let starttime = Date.now();
     const completion = await groq.chat.completions.create({
         messages,
         model,
         temperature: 1,
         max_tokens: 1024,
         top_p:1,
-        stream: false
+        stream: true
     })
-    return completion?.choices[0]?.message?.content || ""
+   let fullMessage = "";
+
+   for await (const chunk of completion) {
+     const chunkTime = (Date.now() - starttime) / 1000;
+     fullMessage += chunk.choices[0].delta.content || "";
+         process.stdout.write(JSON.stringify(chunk.choices[0]?.delta || ""));
+         console.log(" chunk time:", chunkTime);
+   }
+
+   return fullMessage;
 }
